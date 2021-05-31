a6394566da4a3d75d52d1a8060410a35398204a0
hb1113 <hbarami@gmail.com>
2021-05-31 11:19:51 +0100

    create and initial store app

---
+++ store/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from category.models import Category
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='photos/product')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product_name
